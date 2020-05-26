fbc180dc090ac3f44bcccf4f06074dda2befea86
sadhanareddyb <38814193+sadhanareddyb@users.noreply.github.com>
2020-05-26 08:30:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ db.sequelize.sync();
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to DELTA  application." });
 });
 
 require("./app/routes/turorial.routes")(app);
